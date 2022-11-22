09f53524ba2b66627ef0b403d67d727b5f13e073
bgiurgiu7@gmail.com <bgiurgiu7@gmail.com>
2022-11-21 22:44:03 -0700

    Finished Exercise 1

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace MethodsExercise
+﻿using System.Drawing;
+
+namespace MethodsExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // ------------Exercise 1--------------------------------
+
+            Console.WriteLine("Hello, What is your first name?");
+            var userName = Console.ReadLine();
+
+            Console.WriteLine($"Hi {userName}! What is your favorite color?");
+            var color = Console.ReadLine();
+
+            Console.WriteLine($"{color} is an awesome color :) What is your favorite animal?");
+            var animal = Console.ReadLine();
+
+            Console.WriteLine("Great, now what is your favorite band?");
+            var band = Console.ReadLine();
+
+            Console.WriteLine("Here is your mad lib!");
+
+            Console.WriteLine($"{userName} was walking home from school when a " +
+                $"{color} {animal} was spotted across the street performing a " +
+                $"{band} cover");
+            
         }
     }
 }
